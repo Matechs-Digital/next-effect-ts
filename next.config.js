32389acde91250014628a833d8243ab2bd7218eb
Michael Arnaldi <arnaldimichael@gmail.com>
2021-04-05 14:12:38 +0100

    use webpack 5

--- next.config.js
+++ next.config.js
@@ -1,21 +1,30 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    webpack: (config) => {
-        config.module.rules = [{
-            test: /\.(tsx|ts)$/,
-            use: [{
-                loader: 'ts-loader',
-                options: {
-                    configFile: 'tsconfig.build.json',
-                    compiler: 'ttypescript'
-                }
-            }],
-        }, ...config.module.rules.map(r => {
-            if(String(r.test).includes("ts")){
-                return {...r, test: /\.(js|mjs|jsx)$/}
+  webpack: (config) => {
+    config.module.rules = [
+      {
+        test: /\.(tsx|ts)$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: "tsconfig.build.json",
+              compiler: "ttypescript"
             }
-            return r
-        })]
+          }
+        ]
+      },
+      ...config.module.rules.map((r) => {
+        if (String(r.test).includes("ts")) {
+          return { ...r, test: /\.(js|mjs|jsx)$/ }
+        }
+        return r
+      })
+    ]
 
-        return config
-    },
+    return config
+  },
+  future: {
+    webpack5: true
+  }
 }
