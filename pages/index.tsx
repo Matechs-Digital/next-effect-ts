752ab2550a5f65619a0e64e30448db6f0f03e828
Michael Arnaldi <arnaldimichael@gmail.com>
2021-04-29 20:03:16 +0100

    improve demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
+import { pipe } from "@effect-ts/core"
+import { Tagged } from "@effect-ts/core/Case"
+import * as Chunk from "@effect-ts/core/Collections/Immutable/Chunk"
 import * as T from "@effect-ts/core/Effect"
-import * as S from "@effect-ts/core/Effect/Experimental/Stream"
 import * as L from "@effect-ts/core/Effect/Layer"
 import type { Either } from "@effect-ts/core/Either"
-import { pipe } from "@effect-ts/core/Function"
 import type { Has } from "@effect-ts/core/Has"
 import { tag } from "@effect-ts/core/Has"
 import type { _A } from "@effect-ts/core/Utils"
 import { matchTag } from "@effect-ts/core/Utils"
 import * as Q from "@effect-ts/query/Query"
+import * as Schema from "@effect-ts/schema"
+import * as Parser from "@effect-ts/schema/Parser"
 import * as React from "react"
 
 import { createApp as createApp } from "../goods/appEnvironmet"
@@ -24,39 +27,98 @@ export const LiveCalculator = L.fromEffect(Calculator)(makeCalculator)
 
 export const App = createApp<Has<Calculator> & T.DefaultEnv>()
 
+export class HttpError extends Tagged("HttpError")<{
+  readonly error: unknown
+}> {}
+
+export function httpFetch(input: RequestInfo, init?: Omit<RequestInit, "signal">) {
+  const cotroller = new AbortController()
+  const signal = cotroller.signal
+  const params = {
+    signal,
+    ...init
+  }
+
+  return T.tryCatchPromise(
+    () => fetch(input, params).then((data): Promise<unknown> => data.json()),
+    (error) => new HttpError({ error })
+  )
+}
+
+const Commit = Schema.struct({
+  required: {
+    commit: Schema.struct({
+      required: {
+        message: Schema.string
+      }
+    })
+  }
+})
+
+const Commits = Schema.chunk(Commit)
+
+const parseCommits = Parser.for(Commits)["|>"](Schema.condemnFail)
+
 export function Autocomplete() {
-  const [subName, pubName] = App.useHub<string>()
+  const [page, setPage] = React.useState(0)
 
-  const [name] = App.useSubscribe("", subName, [])
+  const commits = App.useQuery(
+    () =>
+      Q.gen(function* (_) {
+        const response = yield* _(
+          httpFetch(`https://api.github.com/repos/Effect-TS/core/commits?page=${page}`)
+        )
 
-  const nameLength = App.useQuery(
-    () => Q.fromEffect(T.succeedWith(() => name.length)["|>"](T.delay(1000))),
-    [name]
+        return yield* _(parseCommits(response))
+      }),
+    [page]
   )
 
-  const renderDone: (_: Either<never, number>) => string = matchTag({
-    Right: (_) => `${_.right}`,
-    Left: (_) => _.left
+  const renderDone: (
+    _: Either<HttpError | Schema.CondemnException, Schema.ParsedShapeOf<typeof Commits>>
+  ) => JSX.Element = matchTag({
+    Right: (_) => (
+      <div>
+        {Chunk.map_(_.right, (_) => (
+          <p>{_.commit.message}</p>
+        ))}
+      </div>
+    ),
+    Left: (_) =>
+      _.left["|>"](
+        matchTag({
+          CondemnException: (_) => (
+            <div>
+              {_.message.split("\n").map((s) => (
+                <>
+                  {s}
+                  <br />
+                </>
+              ))}
+            </div>
+          ),
+          HttpError: () => <div>Error</div>
+        })
+      )
   })
 
   return (
     <div>
-      <input
-        type="text"
-        name="name"
-        onChange={(_) => {
-          pubName(_.target.value)
-        }}
-      />
+      {commits["|>"](
+        matchTag({
+          Done: (_) => _.current["|>"](renderDone),
+          Loading: () => <div>Loading...</div>,
+          Refreshing: (_) => <div>Loading...</div>
+        })
+      )}
       <div>
-        Length:
-        {nameLength["|>"](
-          matchTag({
-            Done: (_) => _.current["|>"](renderDone),
-            Loading: () => "Loading...",
-            Refreshing: (_) => `${_.current["|>"](renderDone)} (Loading...)`
-          })
-        )}
+        <button
+          onClick={() => {
+            setPage((_) => _ + 1)
+          }}
+        >
+          Previous
+        </button>
       </div>
     </div>
   )
