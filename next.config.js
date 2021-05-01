f0fab276e98d2ac04a49c98de0054c8fc49acb85
Michael Arnaldi <arnaldimichael@gmail.com>
2021-05-01 13:09:54 +0100

    temp disable tsc

--- next.config.js
+++ next.config.js
@@ -4,26 +4,26 @@ module.exports = {
     if (config.mode === "development") {
       return config
     }
-    config.module.rules = [
-      {
-        test: /\.(tsx|ts)$/,
-        use: [
-          {
-            loader: "ts-loader",
-            options: {
-              configFile: "tsconfig.build.json",
-              compiler: "ttypescript"
-            }
-          }
-        ]
-      },
-      ...config.module.rules.map((r) => {
-        if (String(r.test).includes("ts")) {
-          return { ...r, test: /\.(js|mjs|jsx)$/ }
-        }
-        return r
-      })
-    ]
+    //config.module.rules = [
+    //  {
+    //    test: /\.(tsx|ts)$/,
+    //    use: [
+    //      {
+    //        loader: "ts-loader",
+    //        options: {
+    //          configFile: "tsconfig.build.json",
+    //          compiler: "ttypescript"
+    //        }
+    //      }
+    //    ]
+    //  },
+    //  ...config.module.rules.map((r) => {
+    //    if (String(r.test).includes("ts")) {
+    //      return { ...r, test: /\.(js|mjs|jsx)$/ }
+    //    }
+    //    return r
+    //  })
+    //]
 
     return config
   },
